71f48b2add52cbf27d03ff19e25ca55ed36b7f7a
JPantana <joshpantana@me.com>
2019-06-24 18:52:46 -0500

    auth and home components created and called

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
+
 import './App.scss';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   
   render() {
     
     return (
-      <button className='btn btn-danger'>Fish Store</button>
-
+      <div className="App">
+        <Auth />
+        <Home />
+      </div>
     );
   }
 }
