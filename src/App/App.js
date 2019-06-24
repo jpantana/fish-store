cd8de3df457b395603967f28cec14d8819851a5f
JPantana <joshpantana@me.com>
2019-06-24 18:28:44 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  
+  render() {
+    
+    return (
+      <button className='btn btn-danger'>Fish Store</button>
+
+    );
+  }
+}
+
+export default App;
