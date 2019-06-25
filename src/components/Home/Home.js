644bc2cad872403d7c78ec17964ff1c24b25a98f
JPantana <joshpantana@me.com>
2019-06-25 18:47:00 -0500

    3 new components printing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
+import Orders from '../Orders/Orders';
+import Inventory from '../Inventory/Inventory';
+import NewOrder from '../NewOrder/NewOrder';
+
 import './Home.scss';
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="Home">
-        <h1>Home</h1>
+      <div className="Home w-100">
+        <div className="container m-auto w-100">
+          <div className="row justify-content-center h-100 w-100">
+            <Inventory />
+            <NewOrder />
+            <Orders />
+          </div>
+        </div>
       </div>
     );
   }
