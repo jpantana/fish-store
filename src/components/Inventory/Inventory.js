1b748b919ec7f38e8df7db93ba7738be7656cef0
JPantana <joshpantana@me.com>
2019-06-25 20:09:34 -0500

    getting fishes back

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import fishData from '../../helpers/data/fishData';
+
 import './Inventory.scss';
 
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+
+  componentDidMount() {
+    fishData.getFishes()
+      .then(fishes => this.setState({ fishes }))
+      .catch(err => console.error('no fish for you', err));
+  }
+
   render() {
     return (
       <div className="Inventory">
